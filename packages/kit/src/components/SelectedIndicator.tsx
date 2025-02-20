17ce2e77a02f684a0f037172ba2c597141d0c307
samuelea <batonenbois@gmail.com>
2025-02-20 12:21:55 -0500

    option select in checkout modal

--- packages/kit/src/components/SelectedIndicator.tsx
+++ packages/kit/src/components/SelectedIndicator.tsx
@@ -6,11 +6,13 @@ interface SelectedIndicatorProps {
 
 export const SelectedIndicator = (props: SelectedIndicatorProps) => {
   const { selected } = props
+  const background = selected ? 'bg-white' : 'bg-transparent'
   return (
     <div
-      className="flex border-solid relative items-center justify-center shrink-0 rounded-full w-7 h-7"
+      className={`flex border-solid relative items-center justify-center shrink-0 rounded-full w-7 h-7 ${background}`}
       style={{
-        borderWidth: '2px'
+        borderWidth: '2px',
+        borderColor: selected ? 'var(--seq-color-white)' : 'var(--seq-color-muted)'
       }}
     >
       {selected && <CheckmarkIcon className="text-black" size="md" />}
